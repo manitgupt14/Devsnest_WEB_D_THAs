e46cecdcbd694722cb3461b54cf444c720f206b7
Nitin Gupta <66675656+manitgupt14@users.noreply.github.com>
2021-07-14 18:55:44 +0530

    Add files via upload

---
+++ WEB_D_ASSIGNMENT/tha_19/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './tha_19';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
