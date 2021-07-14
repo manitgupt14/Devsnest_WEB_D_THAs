cd4b9153373e613c67a61bb0a0d8c518f9282ab2
Nitin Gupta <66675656+manitgupt14@users.noreply.github.com>
2021-07-14 19:16:22 +0530

    Add files via upload

---
+++ WEB_D_ASSIGNMENT/tha_15/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(<div className="first">
+   <h1>React Basics</h1>
+   <h1>Hello World!</h1></div>,
+   document.getElementById('root')
+)
