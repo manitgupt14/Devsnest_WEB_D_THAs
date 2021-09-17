e17bf8ed8f39e7bf5fbea9758915dbb01e1aefb2
Nitin Gupta <66675656+manitgupt14@users.noreply.github.com>
2021-09-17 22:04:14 +0530

    Delete he.js

--- WEB_D_ASSIGNMENT/tha_5_backened/he.js
+++
@@ -1,8 +0,0 @@
-var fs=require("fs");
-
-fs.writeFileSync("day_5/hello.txt","Hello Everyone,");
-
-fs.appendFileSync("day_5/hello.txt"," I am glad to tell you that i have completed this tha and thank you for this course");
-
-fs.readFileSync("day_5/hello.txt","utf-8")
-fs.writeFileSync("day_5/hell.js","console.log('HELLO WORLD')");
